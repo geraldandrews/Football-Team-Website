91b457bc8513cb35752e0c274be6b28b60f4a3bd
Gerald Andrews <gerald2399@comcast.net>
2024-03-20 20:41:16 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-$(document).ready(function () {
-  $(".btn").click(function () {
-    $("nav ul").slideToggle(1000);
-  })
+const btn = document.querySelector('.btn');
+const links = document.querySelector('.nav-links');
+const link = document.querySelectorAll('#page');
 
-})
+hamburger.addEventListener('click', function(e) {
+   hamburger.classList.toggle('cross');
+});
+ 
+btn.addEventListener('click', function () { 
+   if (links.classList.contains('show-links')) {
+       links.classList.remove('show-links');
+   } else {
+      links.classList.add('show-links');
+   }
+
+   event.preventDefault()
+});
